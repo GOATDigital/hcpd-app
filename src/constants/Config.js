2d8383e18de72eebf2338b300c2d29155780c3ad
Mikhail Kuznetcovl <Mikhail.Kuznetcov@ing.nl>
2017-06-18 14:02:43 +0200

    add AWS config, restructure config file

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,8 +1,12 @@
+const HEROKU_API = 'https://hcpd-backend.herokuapp.com';
+const AWS_API = 'http://ec2-34-210-250-51.us-west-2.compute.amazonaws.com:8080';
+
 export const CLIENT_ID = __APPID__.trim();
-export const GOOGLE_MAPS_API = 'AIzaSyC_ITcc0I_2wC21ct9y-He2BNAVRP57JsE';
-export const API_ADDRESS = 'https://hcpd-backend.herokuapp.com';
+
+export const API_ADDRESS = HEROKU_API;
 export const API_USER_ID = '59285539b8deb639861a1e07';
 export const API_USERNAME = 'nea';
 export const API_PASSWORD = 'nea-hcpd';
 export const APP_EMBED_ELEMENT = 'hcpd-container';
+export const GOOGLE_MAPS_API = 'AIzaSyC_ITcc0I_2wC21ct9y-He2BNAVRP57JsE';
 export const GOOGLE_MAPS_API_INLINE = 'AIzaSyBb2cdeOErzIvDQeHqBeCB_RJkgog3U5ic';
